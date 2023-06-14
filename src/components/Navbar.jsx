ef47f09493dc10ef54c6277183a69566b0ef9408
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-14 10:23:05 -0600

    Se cambia el color de fondo de Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 export const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Carro de Compras App</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
