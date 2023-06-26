faedf545d10a9c5364f8cf07a9a8ae0f10389963
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-26 14:13:19 -0600

    Se conecta front end de carro de compras con su back end correspondiente

--- src/components/VistaProductosCat.jsx
+++ src/components/VistaProductosCat.jsx
@@ -5,11 +5,21 @@ import PropTypes from 'prop-types';
 
 export const VistaProductosCat = ({handler}) => {
   const [productos, setProductos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const findAll = async () => {
+    const productosBE = await getProductos();
+    setProductos(productosBE);
+    setIsLoading(false);
+  }
+
   useEffect(() => {
-    setProductos(getProductos());
+    findAll();
   }, []);
+
   return (
     <>
+      {isLoading && <div className="alert aler-info">Cargando ...</div>}
       <div className="row">
         {productos.map((p) => (
           <div className="col-4 my-2" key={p.id}>
--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,11 +1,15 @@
-import { generaProducto, generaProductos, generaCarroItemsVacio } from '../data/productoDatos';
+import { generaProducto, /* generaProductos, */ generaCarroItemsVacio } from '../data/productoDatos';
 
 export const getProducto = () => {
     return generaProducto();
 }
 
-export const getProductos = () => {
-    const productos = generaProductos();
+export const getProductos = async () => {
+    //const productos = generaProductos();
+    //const url = 'http://localhost:8080/api/v1/productos'; //loaclhost
+    const url = 'http://192.168.1.136:8080/api/v1/productos'; //remoto
+    const respuesta = await fetch(url);
+    const productos = await respuesta.json();
     return productos;
 }
 
